fc7f042bb6265574f3d669864c910080b4ff8a92
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-02 09:31:11 -0300

    removi o storyboard e printei o load da view

---
+++ MyMovies/MyMovies/Controller/View/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 02/06/22.
+//
+
+import UIKit
+
+class MovieListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
