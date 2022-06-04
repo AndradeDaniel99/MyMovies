8b3118208f5706346bf605d1573fcfd18554326e
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-03 22:12:37 -0300

    resetei o projeto

---
+++ MyMovies/MyMovies/MovieList/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ MyMovies/MyMovies/MovieList/View/MovieListView.swift
@@ -0,0 +1,14 @@
+//
+//  MovieListView.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 03/06/22.
+//
+
+import UIKit
+
+class MovieListView: UIView {
+
+    
+    
+}
