7d2ce4fad45eb3b0903b1a7e661669b00885251c
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-14 04:20:36 -0300

    moviedetailsviewcontroller

---
+++ MyMovies/MyMovies/MovieList/Controller/MovieDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsViewController.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 14/06/22.
+//
+
+import UIKit
+
+class MovieDetailsViewController {
+
+    let controller: UIViewController
+    
+    init(controller: UIViewController){
+        self.controller = controller
+    }
+
+}
