d01b4562941ddfbcbd0baa4236afae78b83cea2b
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-15 15:21:27 -0300

    moviecollectionview

---
+++ MyMovies/MyMovies/MovieList/View/MovieCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCollectionView.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 15/06/22.
+//
+
+import UIKit
+
+class MovieCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
