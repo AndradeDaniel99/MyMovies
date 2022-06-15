0f2ee62876b11dadc82433cb85cc9ed6d619f2f9
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-15 15:26:05 -0300

    moviecollectionview

--- MyMovies/MyMovies/MovieList/View/MovieCollectionView.swift
+++ MyMovies/MyMovies/MovieList/View/MovieCollectionView.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class MovieCollectionView: UICollectionView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        setupCollectionView()
+        adicionarConstraints()
     }
-    */
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 
 }
