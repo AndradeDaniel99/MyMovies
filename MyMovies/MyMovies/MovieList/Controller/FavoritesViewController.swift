479904e512cd0346e3d226d7375cc9d269af9f7f
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-06-15 23:18:40 -0300

    tabviewcontroller

---
+++ MyMovies/MyMovies/MovieList/Controller/FavoritesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesViewController.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 15/06/22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
---
+++ MyMovies/MyMovies/MovieList/Controller/TabViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabViewController.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 15/06/22.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
