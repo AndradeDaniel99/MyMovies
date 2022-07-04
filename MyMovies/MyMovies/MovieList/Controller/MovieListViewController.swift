243e7aa1e8c5090d5e46262823d161d409061cb3
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-07-04 12:08:47 -0300

    button make my movie working

--- MyMovies/MyMovies/MovieList/Controller/MovieListViewController.swift
+++ MyMovies/MyMovies/MovieList/Controller/MovieListViewController.swift
@@ -106,7 +106,7 @@ extension MovieListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let movie = movies[indexPath.item]
         let selectedMovieViewController = SelectedMovieViewController(movie: movie, buttonBool: false)
-//        selectedMovieViewController.favoritesDelegate = 
+        selectedMovieViewController.favoritesDelegate = self.favoritesDelegate
         navigationController?.pushViewController(selectedMovieViewController, animated: false)
     }
     
