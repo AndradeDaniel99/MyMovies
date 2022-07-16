7fb648169508e59e23185613a3746a7535748e9c
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-07-16 15:59:57 -0300

    login/register

---
+++ MyMovies/MyMovies/App/Welcome/Controller/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  MyMovies
+//
+//  Created by Daniel de Andrade Souza on 16/07/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func register(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        
+    }
+}
